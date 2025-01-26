cceceb3859e51e09a8a6eff0d903aab8d7c3350f
DiegoBB1 <diego.bautistabaez@yahoo.com>
2025-01-25 23:44:30 -0600

    Improved compatibility across mobile devices

--- assets/js/add-content.js
+++ assets/js/add-content.js
@@ -1,14 +1,29 @@
-var today = new Date(); //variable to hold date
-var hourNow = today.getHours(); //variable to get and hold hr of current date
-var greeting; //variable to hold string
-var greetTag = document.getElementById("greetingDiv");
-if(hourNow >18){ //>6
-    greeting = 'Good Evening!';
-} else if (hourNow >12){
-    greeting = 'Good Afternoon!';
-}else if (hourNow >0) {
-    greeting = 'Good Morning!';
-}else{
-    greeting = 'Welcome!';
+document.addEventListener('DOMContentLoaded', () => {
+    const pageType = document.body.dataset.page;
+  
+    if (pageType === 'home')
+      displayGreeting();
+
+  });
+
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('open');
+  });
+  
+function displayGreeting(){
+    var today = new Date(); //variable to hold date
+    var hourNow = today.getHours(); //variable to get and hold hr of current date
+    var greeting; //variable to hold string
+    var greetTag = document.getElementById("greetingDiv");
+    if(hourNow >18){ //>6
+        greeting = 'Good Evening!';
+    } else if (hourNow >12){
+        greeting = 'Good Afternoon!';
+    }else if (hourNow >0) {
+        greeting = 'Good Morning!';
+    }else{
+        greeting = 'Welcome!';
+    }
+    greetTag.innerHTML = '<h1 style="text-align: center">' + greeting + '</h1>'; // assign the value to innerHTML
 }
-greetTag.innerHTML = '<h1 style="text-align: center">' + greeting + '</h1>'; // assign the value to innerHTML
